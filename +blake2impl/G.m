682aa27e0434cbcf943439b9cef93c7a44b61ae5
stefanopradella <stefano.pradella.97@gmail.com>
2026-06-02 23:30:50 +0200

    Add BLAKE2b constants

--- +blake2impl/G.m
+++ +blake2impl/G.m
@@ -10,18 +10,18 @@ function v = G(v, a, b, c, d, x, y)
     v(a) = blake2impl.add64(v(a), v(b));
     v(a) = blake2impl.add64(v(a), x);
     v(d) = bitxor(v(d), v(a));
-    v(d) = blake2impl.ror64(v(d), 32);
+    v(d) = blake2impl.ror64(v(d), Constants.BLAKE2b.G_rc(1));
 
     v(c) = blake2impl.add64(v(c), v(d));
     v(b) = bitxor(v(b), v(c));
-    v(b) = blake2impl.ror64(v(b), 24);
+    v(b) = blake2impl.ror64(v(b), Constants.BLAKE2b.G_rc(2));
 
     v(a) = blake2impl.add64(v(a), v(b));
     v(a) = blake2impl.add64(v(a), y);
     v(d) = bitxor(v(d), v(a));
-    v(d) = blake2impl.ror64(v(d), 16);
+    v(d) = blake2impl.ror64(v(d), Constants.BLAKE2b.G_rc(3));
 
     v(c) = blake2impl.add64(v(c), v(d));
     v(b) = bitxor(v(b), v(c));
-    v(b) = blake2impl.ror64(v(b), 63);
+    v(b) = blake2impl.ror64(v(b), Constants.BLAKE2b.G_rc(4));
 end
