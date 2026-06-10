1dea86097d076f99f4207cb2ccd07b89037a8b40
stefanopradella <stefano.pradella.97@gmail.com>
2026-06-10 23:19:13 +0200

    Fix function interfaces, namespace calls and argument parsing

--- +blake2impl/F.m
+++ +blake2impl/F.m
@@ -1,16 +1,17 @@
-function h = F(h, m, t, f)
+function h = F(h, m, t, f, params)
 % Compression function F, RFC 7693 - 3.2
 
     arguments
-        h                   (8, 1)  uint64
-        m                   (16, 1) uint64
-        t                   (2, 1)  uint64
-        f                   (1, 1)  boolean
+        h       (8, 1)  uint64
+        m       (16, 1) uint64
+        t       (2, 1)  uint64
+        f       (1, 1)  logical
+        params  (1, 1)  struct
     end
 
-    v           =   uint64(16, 1);
+    v           =   zeros(16, 1, 'uint64');
     v(1:8)      =   h;
-    v(9:16)     =   blake2impl.Blake2_Constants.IV;
+    v(9:16)     =   blake2impl.Constants.IV;
 
     v(13)       =   bitxor(v(13), t(1));
     v(14)       =   bitxor(v(14), t(2));
@@ -20,17 +21,20 @@ function h = F(h, m, t, f)
         v(15) = bitxor(v(15), intmax('uint64'));
     end
 
-    sigma = blake2impl.Blake2_Constants.Sigma;
-    for i = 1:12
-        v = G(v, 1, 5, 9,  13, m(sigma(i, 1)), m(sigma(i, 2)));
-        v = G(v, 2, 6, 10, 14, m(sigma(i, 3)), m(sigma(i, 4)));
-        v = G(v, 3, 7, 11, 15, m(sigma(i, 5)), m(sigma(i, 6)));
-        v = G(v, 4, 8, 12, 16, m(sigma(i, 7)), m(sigma(i, 8)));
+    sigma = blake2impl.Constants.Sigma;
+    r     = params.r;
+    R     = params.R;
 
-        v = G(v, 1, 6, 11, 16, m(sigma(i, 9)), m(sigma(i, 10)));
-        v = G(v, 2, 7, 12, 13, m(sigma(i, 11)),m(sigma(i, 12)));
-        v = G(v, 3, 8, 9,  14, m(sigma(i, 13)),m(sigma(i, 14)));
-        v = G(v, 4, 9, 10, 15, m(sigma(i, 15)),m(sigma(i, 16)));
+    for i = 1:r
+        v = blake2impl.G(v, 1, 5, 9,  13, m(sigma(i, 1)), m(sigma(i, 2)), R);
+        v = blake2impl.G(v, 2, 6, 10, 14, m(sigma(i, 3)), m(sigma(i, 4)), R);
+        v = blake2impl.G(v, 3, 7, 11, 15, m(sigma(i, 5)), m(sigma(i, 6)), R);
+        v = blake2impl.G(v, 4, 8, 12, 16, m(sigma(i, 7)), m(sigma(i, 8)), R);
+
+        v = blake2impl.G(v, 1, 6, 11, 16, m(sigma(i, 9)), m(sigma(i, 10)), R);
+        v = blake2impl.G(v, 2, 7, 12, 13, m(sigma(i, 11)),m(sigma(i, 12)), R);
+        v = blake2impl.G(v, 3, 8, 9,  14, m(sigma(i, 13)),m(sigma(i, 14)), R);
+        v = blake2impl.G(v, 4, 5, 10, 15, m(sigma(i, 15)),m(sigma(i, 16)), R);
     end
 
     h(1:8) = bitxor(h(1:8), bitxor(v(1:8), v(9:16)));
--- +blake2impl/G.m
+++ +blake2impl/G.m
@@ -1,27 +1,29 @@
-function v = G(v, a, b, c, d, x, y)
+function v = G(v, a, b, c, d, x, y, R)
 % Mixing function G, RFC 7693 - 3.1
 % BLAKE2b rotation constants are hardcoded
 
     arguments
         v                   (16, 1) uint64
-        a, b, c, d, x, y    (1, 1)  uint64
+        a, b, c, d          (1, 1)  uint8
+        x, y                (1, 1)  uint64
+        R                   (4, 1)  uint8
     end
 
     v(a) = blake2impl.add64(v(a), v(b));
     v(a) = blake2impl.add64(v(a), x);
     v(d) = bitxor(v(d), v(a));
-    v(d) = blake2impl.ror64(v(d), Constants.BLAKE2b.G_rc(1));
+    v(d) = blake2impl.ror64(v(d), R(1));
 
     v(c) = blake2impl.add64(v(c), v(d));
     v(b) = bitxor(v(b), v(c));
-    v(b) = blake2impl.ror64(v(b), Constants.BLAKE2b.G_rc(2));
+    v(b) = blake2impl.ror64(v(b), R(2));
 
     v(a) = blake2impl.add64(v(a), v(b));
     v(a) = blake2impl.add64(v(a), y);
     v(d) = bitxor(v(d), v(a));
-    v(d) = blake2impl.ror64(v(d), Constants.BLAKE2b.G_rc(3));
+    v(d) = blake2impl.ror64(v(d), R(3));
 
     v(c) = blake2impl.add64(v(c), v(d));
     v(b) = bitxor(v(b), v(c));
-    v(b) = blake2impl.ror64(v(b), Constants.BLAKE2b.G_rc(4));
+    v(b) = blake2impl.ror64(v(b), R(4));
 end
